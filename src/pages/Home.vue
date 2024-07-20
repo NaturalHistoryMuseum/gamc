be97ae363afc1dda031f27c7c50c3d9c5025267c
Josh Humphries <j.humphries@nhm.ac.uk>
2024-07-20 15:56:09 +0100

    feat: implement keyboard based selection on dataset search

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,21 +7,21 @@
         placeholder="Choose a GBIF occurrence dataset..."
         v-model="search"
         ref="searchInput"
+        @keydown.down.prevent="highlightNext"
+        @keydown.up.prevent="highlightPrev"
+        @keydown.enter="select(datasets[highlightedIndex])"
+        @keydown.esc="search = ''"
       />
-      <ul
-        v-if="datasets.length > 0"
-        class="absolute h-48 overflow-y-auto w-full"
-      >
+      <ul v-if="datasets.length > 0" class="absolute w-full">
         <li
-          v-for="dataset in datasets"
+          v-for="(dataset, index) in datasets"
           :key="dataset.key"
-          class="cursor-pointer hover:bg-amber-100 px-2 py-0.5"
+          class="cursor-pointer px-2 py-0.5"
+          :class="{ 'bg-amber-100': highlightedIndex === index }"
+          @click="select(dataset)"
+          @mouseover="highlightedIndex = index"
         >
-          <RouterLink
-            :to="{ name: 'dashboard', params: { datasetKey: dataset.key } }"
-          >
-            {{ dataset.label }}
-          </RouterLink>
+          {{ dataset.label }}
         </li>
       </ul>
     </div>
@@ -31,10 +31,14 @@
 <script setup>
 import { onMounted, ref, watch } from 'vue';
 import { searchDatasets } from '../lib/gbifApi.js';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const searchInput = ref(null);
 const search = ref(null);
 const datasets = ref([]);
+const highlightedIndex = ref(0);
 
 // for debouncing
 let timer;
@@ -48,13 +52,29 @@ watch(search, async () => {
   // debounce the search so that we don't hammer GBIF
   clearTimeout(timer);
   timer = setTimeout(async () => {
-    const results = await searchDatasets(search.value);
+    const results = await searchDatasets(search.value, 10);
     datasets.value = results.map((result) => ({
       label: result.title,
       key: result.key,
     }));
+    highlightedIndex.value = 0;
   }, 100);
 });
+
+async function select(dataset) {
+  await router.push({ name: 'dashboard', params: { datasetKey: dataset.key } });
+}
+
+function highlightNext() {
+  highlightedIndex.value = (highlightedIndex.value + 1) % datasets.value.length;
+}
+
+function highlightPrev() {
+  if (highlightedIndex.value === 0) {
+    highlightedIndex.value = datasets.value.length;
+  }
+  highlightedIndex.value = (highlightedIndex.value - 1) % datasets.value.length;
+}
 
 onMounted(() => {
   searchInput.value.focus();
