c9000570b581b744813fb31b73d3b6ae8be9d7dc
Josh Humphries <j.humphries@nhm.ac.uk>
2024-07-21 10:11:48 +0100

    refactor: use a named path for link from 404 to home

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -4,7 +4,7 @@
       <p class="text-4xl">Page not found</p>
     </div>
     <div class="p-2">
-      <RouterLink to="/">Go home</RouterLink>
+      <RouterLink :to="{ name: 'home' }">Go home</RouterLink>
     </div>
   </div>
 </template>
